ee7d6277d23b0ac2abd38c2c1dd1ea5e62ab398f
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-06-08 00:48:35 +0200

    add footer

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -7,34 +7,59 @@ import clsx from 'clsx';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
 import styles from './Footer.module.scss';
+import { Logo } from '../../common/Logo/Logo';
+
 
 const Component = ({classNameName, children}) => (
-  <div classNameName={clsx(classNameName, styles.root)}>
-    <footer>
-      <div className="container">
-        <div className="row align-items-baseline">
-          <div className="col-md-6 mb-4 ">
-            <a className="fb-ic">
-              <i className="fab fa-facebook-f fa-lg  mr-md-5 mr-3 fa-2x "> </i>
+  <footer classNameName={clsx(classNameName, styles.root)}>
+      <div className='container-fluid d-flex'>
+        <div className={`${styles.footerSocial} col-md-6 text-center p-5`}>
+            <Logo />
+          <div className="footer-icons mt-3 d-flex justify-content-around flex-wrap ">
+            <a href="#" className="footer-icon">
+              <i className="fab fa-facebook-f "> </i>
             </a>
-            <a className="tw-ic">
-              <i className="fab fa-twitter fa-lg mr-md-5 mr-3 fa-2x "> </i>
+            <a href="#" className="footer-icon">
+              <i className="fab fa-twitter "> </i>
             </a>
-            <a className="ins-ic">
-              <i className="fab fa-instagram fa-lg mr-md-5 mr-3 fa-2x "> </i>
+            <a href="#" className="footer-icon">
+              <i className="fab fa-instagram "> </i>
             </a>
-            <a className="pin-ic ">
-              <i className="fab fa-pinterest fa-lg fa-2x "> </i>
+            <a href="#" className="footer-icon">
+              <i className="fab fa-pinterest "> </i>
             </a>
-          </div>
-          <div className="col-md-6 mb-4 ">
           </div>
         </div>
+        <div className='col-md-6 footer-contact text-center p-5'>
+          <h3 className={styles.contact}>contact</h3>
+          <p className='d-flex flex-wrap'>
+            <span className='m-r footer-icon'>
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+            </span>
+            <span className='m-r footer-icon'>
+              ul. Ptasia 5, 57-222 Kraków
+            </span>
+          </p>
+          <p className='d-flex flex-wrap'>
+            <span className='m-r footer-icon'>
+              <i class="fa fa-phone" aria-hidden="true"></i>
+            </span>
+            <span className='m-r footer-phone'>
+              609-333-938
+            </span>
+          </p>
+          <p className='d-flex flex-wrap'>
+            <span className='m-r footer-icon'>
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+            </span>
+            <span className='m-r footer-icon'>
+              rainbbowl@.gmail.com
+            </span>
+          </p>
+        </div>
       </div>
-    </footer>
-
     {children}
-  </div>
+  </footer>
 );
 
 Component.propTypes = {
--- client/src/components/layout/MainLayout/MainLayout.js
+++ client/src/components/layout/MainLayout/MainLayout.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
 import { Header } from '../Header/Header';
-import { Footer }   from '../Footer/Footer';
+import { Logo } from '../../common/Logo/Logo';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
@@ -13,13 +13,68 @@ import styles from './MainLayout.module.scss';
 
 const Component = ({className, children}) => (
   <div className={clsx(className, styles.root)}>
-    <div>
-      <Header />
-      {children}
-      {/* <div className={styles.footer}>
-        <Footer />
-      </div> */}
-    </div>
+    <Header />
+    {children}
+    <footer>
+    <div className={`${styles.footer} container-fluid footer`}>
+      <div className='row '>
+        <div className= 'col-md-6 text-center p-5' >
+            <div className={`${styles.icons} mt-3 d-flex justify-content-around flex-wrap `}>
+              <ul >
+                <li>
+                    <a href="#">
+                        <i className="fab fa-facebook-f"></i> Facebook
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="fab fa-instagram"></i> Instagram
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="fab fa-twitter"></i> Behance
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className="fab fa-pinterest"></i> Behance
+                    </a>
+                </li>
+                </ul>
+            </div>
+          </div>
+        <div className={`${styles.contact} col-md-6 text-center p-5 `}>
+          <h3>Contact</h3>
+          <p className={`${styles.contactIcon} d-flex flex-wrap`}>
+            <span className='m-r' >
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+            </span>
+            <span className='m-r' >
+              ul. Ptasia 5, 57-222 Kraków
+            </span>
+          </p>
+          <p className={`${styles.contactIcon} d-flex flex-wrap`}>
+          <span className='m-r' >
+              <i class="fa fa-phone" aria-hidden="true"></i>
+            </span>
+            <span className='m-r footer-phone'>
+              609-333-938
+            </span>
+          </p>
+          <p className={`${styles.contactIcon} d-flex flex-wrap`}>
+          <span className='m-r' >
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+            </span>
+            <span className='m-r' >
+              rainbbowl@.gmail.com
+            </span>
+          </p>
+        </div>
+      </div>
+
+      </div>
+    </footer>
   </div>
 );
 
